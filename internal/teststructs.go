551d8d61ef801cddc7c56c698a9603c93b27ceae
LiamABailey <bailey.liam102@gmail.com>
2021-09-11 14:39:19 -0400

    Wrote Fruit structs, mongo interaction components

--- internal/teststructs.go
+++ internal/teststructs.go
@@ -5,9 +5,22 @@ import (
   "go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type Fruit struct {
+// when writing a fruit to the DB, we allow mongod to add the _id field
+type FruitWrtiable struct {
+  Name    string              `bson:"name"`
+  Origin  string              `bson:"origin"`
+  Price   int                 `bson:"number"`
+}
+
+// when reading, we recieve the _id field
+type FruiReadable struct {
   ID      primitive.ObjectID  `bson:"_id"`
   Name    string              `bson:"name"`
   Origin  string              `bson:"origin"`
   Price   int                 `bson:"number"`
+}
+
+
+func (f FruitReadable) Copy() FruitReadable {
+  return FruitReadable{f.ID, f.Name, f.Origin, f.Price}
 }
